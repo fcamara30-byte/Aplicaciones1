2e03b4c571e83b014f2d89729b947866bfe5f02e
fcamara30-byte <fcamara30@gmail.com>
2026-06-01 14:36:30 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -1,63 +1,46 @@
-
 import math
 
-# ---------------------------
-# PRESION
-# ---------------------------
-def presion(z, P_superficie, densidad, fill):
+def presion(z, P_surface, rho, fill):
     g = 9.81
-    psi_por_pa = 1 / 6894.76
-    return P_superficie + (densidad * g * z * fill) * psi_por_pa
+    psi_pa = 1 / 6894.76
+    return P_surface + rho * g * z * fill * psi_pa
 
 
-# ---------------------------
-# TENSION CIRCUNFERENCIAL
-# ---------------------------
-def tension_circunferencial(DeltaP, OD, t):
-    return (DeltaP * OD) / (2 * t) / 1000  # ksi
+def tension_circunferencial(dP, OD, t):
+    return (dP * OD) / (2 * t) / 1000
 
 
-# ---------------------------
-# TENSION AXIAL
-# ---------------------------
 def tension_axial(z, OD, ID, rho_int, rho_ext):
     g = 9.81
 
-    area_ext = math.pi * (OD**2) / 4
-    area_int = math.pi * (ID**2) / 4
-    area = area_ext - area_int
+    A_ext = math.pi * OD**2 / 4
+    A_int = math.pi * ID**2 / 4
+    A = A_ext - A_int
 
-    rho_acero = 7850
+    rho_steel = 7850
 
-    masa = area * rho_acero
-    empuje = area_ext * rho_ext - area_int * rho_int
+    masa = A * rho_steel
+    buoy = A_ext * rho_ext - A_int * rho_int
 
-    fuerza = (masa - empuje) * g * z
+    F = (masa - buoy) * g * z
+    sigma = F / A
 
-    sigma = fuerza / area
-
-    return sigma / 6894.76 / 1000  # ksi
+    return sigma / 6894.76 / 1000
 
 
-# ---------------------------
-# TORSION
-# ---------------------------
-def tension_torsion(T_lbft, OD, ID):
-    # lb-ft → lb-in
+def torsion(T_lbft, OD, ID):
     T = T_lbft * 12
 
     ro = OD / 2
     ri = ID / 2
 
-    J = (math.pi / 2) * (ro**4 - ri**4)
+    J = math.pi/2 * (ro**4 - ri**4)
 
-    tau = (T * ro) / J  # psi
+    tau = T * ro / J
 
-    return tau / 1000  # ksi
+    return tau / 1000
 
 
-# ---------------------------
-# VON MISES
-# ---------------------------
 def von_mises(sig_ax, sig_hoop, tau):
-    return math.sqrt(sig_ax**2 + sig_hoop**2 - sig_ax*sig_hoop + 3*(tau**2))
+    return (sig_ax**2 + sig_hoop**2 - sig_ax*sig_hoop + 3*tau**2)**0.5
+
