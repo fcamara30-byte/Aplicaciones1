2d69ebaa037fc26d8bc4553a145a304c644bac32
fcamara30-byte <fcamara30@gmail.com>
2026-06-06 01:56:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -404,13 +404,13 @@ for i in range(200):
 # distribución progresiva de carga externa (sin romper modelo)
     if F_ext < 0:
        F_ext_eff = F_ext * (1 - z / depth_ft)
-     else:
+    else:
        F_ext_eff = F_ext
 
-       F_total = (
-       F_weight
-      - F_buoy
-      + F_ext_eff
+    F_total = (
+     F_weight
+    - F_buoy
+    + F_ext_eff
 )
 
 
