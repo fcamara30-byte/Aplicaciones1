6dff1d5a161501f6329d4d1e39584f8a6d358d61
fcamara30-byte <fcamara30@gmail.com>
2026-06-16 10:24:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -422,7 +422,7 @@ for row in TABLA_TORQUE:
         row["DE"] == size_tubo and
         row["grado"] == grado and
         row["rosca"] == tipo_rosca_clean and
-        abs(row["lbft"] - peso) < 0.3   # matching peso
+        abs(row["lbft"] - peso) < 0.6   # matching peso
     ):
         torque_max_rosca = row["max"]
 
